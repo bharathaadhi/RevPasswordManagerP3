78534e150a78eee7dabb08e59fade7064ce318de
bharathaadhi <bharathaadhi8@gmail.com>
2026-03-08 16:41:33 +0530

    Updated Develop conflicts

--- security-service/src/main/java/com/rev/security/controller/SecurityController.java
+++ security-service/src/main/java/com/rev/security/controller/SecurityController.java
@@ -3,7 +3,11 @@ package com.rev.security.controller;
 import com.rev.security.dto.*;
 import com.rev.security.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
